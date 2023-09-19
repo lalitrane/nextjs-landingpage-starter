2b70b62ad8449de7b34c1e81511ed965ae34ef03
Lalit <lalit@Lalits-MacBook-Pro.local>
2023-09-20 09:35:28 +1200

    dev started

---
+++ app/page.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import NavBar from '@/components/NavBar'
+export default function Home() {
+  return (
+    <>
+
+      <main className="">
+        <NavBar />
+        <div className=" relative h-96 w-full ">
+
+
+          <Image
+            src="/pexels-lalit-rane-395611.jpg"
+            alt="Banner"
+            objectFit='cover'
+            objectPosition='cover'
+            fill
+            // sizes="100vw"
+            className="w-full"
+
+            priority
+          />
+          {/* Photo by Lalit Rane: https://www.pexels.com/photo/astrophotography-auckland-long-exposure-milky-way-395611/ */}
+
+        </div>
+        <div className="container mx-auto">
+          <div className=' m-5'>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+
+
+
+      </main>
+    </>
+  )
+}
